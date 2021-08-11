bdf455dd816a1b50bc7a777a8001529e4ded7604
Elias Alberts <nnhw667@gmail.com>
2021-08-11 18:55:51 +0300

    json refactored read

--- src/module_jerom_the_mavlink.cpp
+++ src/module_jerom_the_mavlink.cpp
@@ -5,6 +5,7 @@
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include "link_factory.h"
 
 namespace
 {
@@ -12,6 +13,7 @@ constexpr char path[] = "./link_config.json";
 } // namespace
 
 using namespace jerom_mavlink::app;
+using namespace loodsman;
 
 ModuleJeromTheMavlink::ModuleJeromTheMavlink()
 {
@@ -28,6 +30,18 @@ void ModuleJeromTheMavlink::init()
 //    qDebug() << doc["port"].toInt();
 //    qDebug() << doc["type"].toString();
 
+    link_ptr link_listen;
+    link_type l_type;
+
+    if (doc["type"].toString() == "udp")
+        l_type = link_type::udp;
+
+    int result = factory(link_listen,l_type, doc["port"].toInt());
+    Q_ASSERT(result == 0);
+
+//    std::string received_data(link_listen->receive());
+//
+//    qDebug() << received_data.c_str();
 
 }
 
