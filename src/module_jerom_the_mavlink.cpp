a3cd1c318e6fdcb119d49b13622be7efb9f11533
Elias Alberts <nnhw667@gmail.com>
2021-08-11 17:46:59 +0300

    link config json deploy and read

--- src/module_jerom_the_mavlink.cpp
+++ src/module_jerom_the_mavlink.cpp
@@ -1,6 +1,15 @@
 #include "module_jerom_the_mavlink.h"
 
 #include <QDebug>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+namespace
+{
+constexpr char path[] = "./link_config.json";
+} // namespace
 
 using namespace jerom_mavlink::app;
 
@@ -10,6 +19,16 @@ ModuleJeromTheMavlink::ModuleJeromTheMavlink()
 
 void ModuleJeromTheMavlink::init()
 {
+    QFile file(::path);
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+
+    QJsonDocument doc = QJsonDocument::fromJson(file.readAll());
+    file.close();
+
+//    qDebug() << doc["port"].toInt();
+//    qDebug() << doc["type"].toString();
+
+
 }
 
 void ModuleJeromTheMavlink::done()
